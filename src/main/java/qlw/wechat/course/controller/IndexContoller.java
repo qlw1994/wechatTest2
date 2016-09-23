709d9702dfb6d95ad30115e677a14b684c9d065e
qlw1994 <epcs1si8>
2016-09-23 13:49:04 +0800

    cominit

---
+++ src/main/java/qlw/wechat/course/controller/IndexContoller.java
@@ -0,0 +1,80 @@
+package qlw.wechat.course.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import qlw.wechat.course.service.CoreService;
+import qlw.wechat.course.util.SignUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@Controller
+@RequestMapping(value = "/wechat")
+public class IndexContoller {
+    private static Logger log = LoggerFactory.getLogger(IndexContoller.class);
+
+    @Autowired
+    CoreService coreService;
+
+    /**
+     * 确认请求来自微信服务器
+     */
+    @RequestMapping(value = "index")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/hello")
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getMethod().equals("GET")) {
+            // 微信加密签名
+            String signature = request.getParameter("signature");
+            // 时间戳
+            String timestamp = request.getParameter("timestamp");
+            // 随机数
+            String nonce = request.getParameter("nonce");
+            // 随机字符串
+            String echostr = request.getParameter("echostr");
+
+            PrintWriter out = response.getWriter();
+            // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+            log.info("request method = " + request.getMethod());
+            if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+                out.print(echostr);
+                System.out.println("微信服务验证成功！");
+
+                out.close();
+                out = null;
+                return;
+            }
+        }
+        /**
+         * 处理微信服务器发来的消息
+         */
+        log.info("处理微信服务器发来的消息");
+        // 将请求、响应的编码均设置为UTF-8（防止中文乱码）
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        // 调用核心业务类接收消息、处理消息
+        String respMessage = coreService.processRequest(request);
+
+        // 响应消息
+        PrintWriter out = response.getWriter();
+        out.print(respMessage);
+
+        out.close();
+        out = null;
+    }
+
+
+}
